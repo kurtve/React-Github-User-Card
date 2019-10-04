045fa23b381208cf1a641ea79148dc3d24c24ad5
Kurt Van Etten <kurt@vanetten.us>
2019-10-03 21:40:07 -0400

    Project turned into dogs instead :)

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 
 const AppWrapper = styled.div`
-
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 
 	h1 {
 		font-size: 6rem;
+	}
+
+	h3 {
+		font-size: 3rem;
+	}
+
+	img {
+		height: 100px;
+		width: auto;
+		margin: 5px;
 	}
 
 `;
@@ -19,7 +29,41 @@ class App extends Component {
 	constructor() {
 		super();
 
-		this.state = { greeting: 'Hello, World!' };
+		this.state = {
+			breed: 'husky',
+			images: []
+		};
+	}
+
+
+	componentDidMount() {
+		this.fetchDogs();
+	}
+
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.breed !== this.state.breed) {
+			this.setState({images: []});
+			this.fetchDogs();
+		}
+	}
+
+
+	handleChange = (event) => {
+		this.setState({
+			breed: event.target.value
+		});
+	};
+
+
+	fetchDogs = () => {
+		axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+			.then(result => {
+				this.setState({...this.state, images: result.data.message});
+			})
+			.catch(error => {
+				console.log(error);
+			});	
 	}
 
 
@@ -27,7 +71,26 @@ class App extends Component {
 		return (
 			<AppWrapper>
 				<div className='main'>
-					<h1>{this.state.greeting}</h1>
+					<h1>Dogs!</h1>
+					<h3>Breed: {this.state.breed}</h3>
+
+					<select value={this.state.breed} onChange={this.handleChange} >
+						<option value='husky'>Husky</option>
+						<option value='beagle'>Beagle</option>
+						<option value='corgi'>Corgi</option>
+					</select>
+
+					<div>
+						{this.state.images.map((image, idx) => {
+							return (
+								<img key={idx}
+									 src={image}
+									 alt={`dog ${idx}`}
+								/>
+							);
+						})}
+					</div>
+
 				</div>
 			</AppWrapper>
 		);
